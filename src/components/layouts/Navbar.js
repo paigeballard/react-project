c1ebc8a99a0559de3ce245027ad8c749ade4c4ba
Paige Ballard <p.ballardhou@gmail.com>
2020-01-04 13:44:38 -0600

    Test of deployment.

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Navbar () {
   return (
     <nav className='navbar navbar-expand-lg navbar-light bg-dark'>
-      <Link className='navbar-brand text-white' to='/home'>cPanel</Link>
+      <Link className='navbar-brand text-white' to='/home'><h1>cPanel</h1></Link>
       <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
         <span>
           <i className='fas fa-bars' style={{ color: '#fff' }} />
